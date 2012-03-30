d4f81d965fd8b5e601a6f8d82188b6f24b4f0f92
Andrew Fallows <kaldrenon@gmail.com>
2012-03-30 15:15:22 -0400

    removed rvm install from second script (it's in the first one now); fixed typos in readme

--- install-chef-server.sh
+++ install-chef-server.sh
@@ -74,10 +74,6 @@ sudo rabbitmqctl set_permissions -p /chef chef ".*" ".*" ".*"
 sudo rabbitmq-plugins enable rabbitmq_management
 sudo service rabbitmq-server restart
 
-# tell rvm to install ruby 1.9.2 as chef and to make it default
-rvm install 1.9.2
-rvm use 1.9.2 --default
-
 # install the chef gems (if we don't already have them)
 for gem in chef-server chef-server-api chef-solr chef-server-webui 
 do
